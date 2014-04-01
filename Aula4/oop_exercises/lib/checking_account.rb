8def121dd187b526724a034abe0e0fe131c2df69
vagrant <andremfq@gmail.com>
2014-04-01 20:54:26 -0300

    add exercise 4.2

---
+++ Aula4/oop_exercises/lib/checking_account.rb
@@ -0,0 +1,26 @@
+require_relative 'bank_account'
+
+class CheckingAccount < BankAccount
+
+  TARIFA=8
+
+  def deposit(amount)
+    @balance += amount
+    log_transaction('Deposit', amount)
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+    log_transaction('Withdrawal', amount)
+  end
+
+  def transfer(account, amount)
+    if(@balance>=amount+TARIFA)
+      
+      self.withdraw(amount+TARIFA)
+      account.deposit(amount)
+      
+    end 
+  end
+
+end
